a35004a2da7405beb80dc37cf93657f470d7e16c
Vlad Mocanu <vladmoca90@gmail.com>
2026-06-03 23:22:01 +0100

    Created a counter.tsx file for the styles only

--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -21,7 +21,7 @@ const indexContent = `import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>Edit src/app/counter/index.tsx to edit this screen.</Text>
     </View>
   );
 }
